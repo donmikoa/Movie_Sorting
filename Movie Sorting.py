b2c3c6d188953850cad75788b3a7a82c3cc3d8e2
michaeliyaomolere <mikoa04@gmail.com>
2019-03-14 09:14:24 -0400

    Initial commit

---
+++ Movie Sorting.py
@@ -0,0 +1,17 @@
+#This program has a list of some movies which will be sorted out based on some filters
+
+moviesDict = {2005: [['Munich', 'Steven Spielberg']],
+              2006: [['The Prestige', 'Christopher Nolan'], ['The Departed', 'Martin Scorsese']],
+              2007: [['Into the Wild', 'Sean Penn']],
+              2008: [['The Dark Knight', 'Christopher Nolan']],
+              2009: [['Mary and Max', 'Adam Elliot']],
+              2010: [["The King's Speech", 'Tom Hooper']],
+              2011: [['The Artist', 'Michel Hazanavicius'], ['The Help', 'Tate Taylor']],
+              2012: [['Argo', 'Ben Affleck']],
+              2013: [['12 Years a Slave', 'Steve McQueen']],
+              2014: [['Birdman', 'Alejandro G. Inarritu']],
+              2015: [['Spotlight', 'Tom McCarthy']],
+              2016: [['The BFG', 'Steven Spielberg']]}
+
+
+
